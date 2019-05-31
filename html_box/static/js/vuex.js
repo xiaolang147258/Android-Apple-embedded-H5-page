92686d30a4612ce2bdb53fd2d4b57802b9a80670
xiaolang147258 <2443611475@qq.com>
2019-05-31 15:24:17 +0800

    a

---
+++ html_box/static/js/vuex.js
@@ -0,0 +1,18 @@
+
+
+
+var Main_domain_name='http://teachers.cieo.com.cn';//主域名http://teachers.cieo.com.cn
+
+// var Main_domain_name='http://teacher-dev47.cieo.com.cn';//测试的域名
+
+//
+
+var Review=0;//点评判断，0代表全班点评，1代表学生点评
+
+//课堂点评，学生头像集合
+
+
+
+
+
+
